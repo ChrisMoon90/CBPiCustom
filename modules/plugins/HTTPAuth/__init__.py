40e7d7a8af46e2780d736eb6c78139182fb7c45e
ChrisMoon90 <chrismoon90@gmail.com>
2020-02-11 21:43:28 -0800

    First Commit

---
+++ modules/plugins/HTTPAuth/__init__.py
@@ -0,0 +1,22 @@
+from modules import app, cbpi
+try:
+  from flask_basicauth import BasicAuth
+except:
+  import os
+  os.system('pip install Flask-BasicAuth')
+  from flask_basicauth import BasicAuth
+
+basic_auth = BasicAuth(app)
+def auth_password():
+  password = cbpi.get_config_parameter('auth_password', None)
+  if password is None:
+    cbpi.add_config_parameter("auth_password", "password", "text", "HTTP Auth password")
+    return "password"
+  else:
+    return password
+
+@cbpi.initalizer(order=1)
+def init_basic_auth(self):
+  app.config['BASIC_AUTH_USERNAME'] = 'admin'
+  app.config['BASIC_AUTH_PASSWORD'] = auth_password()
+  app.config['BASIC_AUTH_FORCE'] = True
